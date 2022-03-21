ef51cb7326c691d5a573856aa7b7826c1f775fc1
harshit singhal <harshitw3b@gmail.com>
2022-03-21 13:52:12 +0530

    s

--- src/App.js
+++ src/App.js
@@ -5,13 +5,39 @@ import Header from './Components/Header'
 import Sidebar from './Components/sidebar'
 //import FileContainer from './Components/FileContainer'
 
+function Test() {
+  let temp = {
+    name:"n",
+    heading:"h",
+    data:"d"
+  };
+  fetch('http://localhost:8080/putFiles', {
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(temp)
+  }).then(res=> {
+    res.json().then(d=>{
+      console.log(d)
+    });
+  });
+  /*return (
+    <span>a</span>
+  );*/
+}
+Test();
+
 function App() {
   
   return (
     <div className="App">
       <Header/>
       <Sidebar/>
+<<<<<<< Updated upstream
 {/* <FileContainer/> */}
+=======
+>>>>>>> Stashed changes
       {/* <FilesView/> */}
     </div>
   );

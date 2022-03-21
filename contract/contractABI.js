ddc4248f46daf9d7a87f12c49d6d9d7b4a6a27c8
harshit singhal <harshitw3b@gmail.com>
2022-03-21 13:51:39 +0530

    c

---
+++ contract/contractABI.js
@@ -0,0 +1,305 @@
+exports.jsonInterface = [
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "change",
+				"type": "uint256"
+			}
+		],
+		"name": "changeFeeValue",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newAdmin",
+				"type": "address"
+			}
+		],
+		"name": "changeOwner",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "newFees",
+				"type": "uint256"
+			}
+		],
+		"name": "feeChanged",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "timestamp",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "old",
+				"type": "string"
+			}
+		],
+		"name": "oldData",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "ownerChanged",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "headings",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "d",
+				"type": "string"
+			}
+		],
+		"name": "putFiles",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "timestamp",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "headings",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "d",
+				"type": "string"
+			}
+		],
+		"name": "saved",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address payable",
+				"name": "recipient",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "sendMoney",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "timestamp",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "update",
+				"type": "string"
+			}
+		],
+		"name": "updated",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "data",
+				"type": "string"
+			}
+		],
+		"name": "updateFileData",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			}
+		],
+		"name": "updateFileName",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getFiles",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "uint256",
+						"name": "timestamp",
+						"type": "uint256"
+					},
+					{
+						"internalType": "string",
+						"name": "name",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "headings",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "data",
+						"type": "string"
+					}
+				],
+				"internalType": "struct MPR.File[]",
+				"name": "",
+				"type": "tuple[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getFilesLength",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getOwner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "misc",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "viewBal",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "viewFee",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
---
+++ server.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
+
+
+const abi = require("./contract/contractABI.js");  // Contract ABI  // need to change
+const contract_Address = "0x300244145E694a26F6634fb8681B0798F5721865"; // need to change
+const rop = "https://ropsten.infura.io/v3/d4d2fd55b14b4e4fa74ccdfd817d681a";  // provider (infura)
+const Web3 = require('web3');
+
+var Contract = require('web3-eth-contract');  // contract method
+Contract.setProvider(rop);  // setting contract provider
+var contract = new Contract(abi.jsonInterface, contract_Address);  // connecting abi
+
+var Tx = require('ethereumjs-tx').Transaction;
+const provider = new Web3.providers.HttpProvider(rop);
+const web3 = new Web3(provider);
+
+const account1 = "0xE405a7C434ba824B9Ad3551C9f6c030e5094B7b4"; // Your account address 1
+//const account2 = '' // Your account address 2
+web3.eth.defaultAccount = account1;
+const privateKey1 = Buffer.from('ed8ab747f2b3ced2dfb2c737fa50852b49fb2ddf6aa9623bf7ecb896cd9e40c8', 'hex');
+
+async function TransferERC20Token(name,heading,filedata) {
+    return new Promise(function (resolve, reject) {
+        try {
+            web3.eth.getBlock("latest", false, (error, result) => {
+                var _gasLimit = result.gasLimit;
+
+                web3.eth.getGasPrice(function (error, result) {
+                    var _gasPrice = result;
+                    try {
+                        var _hex_gasLimit = web3.utils.toHex((3141592).toString());
+                        var _hex_gasPrice = web3.utils.toHex(_gasPrice.toString());
+                        var _hex_Gas = web3.utils.toHex('60000');
+
+                        web3.eth.getTransactionCount(account1, (err, txCount) => {
+                        	console.log(txCount)
+                            const rawTx = {
+                            	nonce: web3.utils.toHex(txCount),
+                                from: account1,
+                                to: contract_Address,
+                                gasPrice: _hex_gasPrice,
+                                gasLimit: _hex_gasLimit,
+                                gas: _hex_Gas,
+                                value: "0x0",
+                                data: contract.methods.putFiles("name","heading","filedata").encodeABI()
+                            };
+
+                            const tx = new Tx(rawTx, { 'chain': 'ropsten' });
+                            tx.sign(privateKey1);
+
+                            var serializedTx = '0x' + tx.serialize().toString('hex');
+                            web3.eth.sendSignedTransaction(serializedTx, function (err, hash) {
+                                if (err) {
+                                    reject(err);
+                                }
+                                else {
+                                    console.log(hash)
+                                    resolve(hash);
+                                }
+                            })
+                        });                                
+                    } catch (error) {
+                        reject(error);
+                    }
+                });
+            });
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+app.post("/putFiles", (req, res)=> {
+	const {name,heading,data} = req.body;
+    console.log(name)
+	TransferERC20Token(name, heading, data);
+	/*contract.methods.putFiles(name,heading,data).send({from:"0xE405a7C434ba824B9Ad3551C9f6c030e5094B7b4"}).then(d=> {
+		res.end(JSON.stringify({count:d}));
+	});*/
+});
+app.get("/getFilesLength", (req, res)=> {
+	contract.methods.getFilesLength().call({from:"0xE405a7C434ba824B9Ad3551C9f6c030e5094B7b4"}).then(d=> {
+		res.end(JSON.stringify({count:d}));
+	});
+});
+app.get("/getFiles", (req, res)=> {
+	contract.methods.getFiles().call({from:"0xE405a7C434ba824B9Ad3551C9f6c030e5094B7b4"}).then(d=> {
+		res.end(JSON.stringify({count:d}));
+	});
+});
+
+
+app.listen(8080, ()=> {
+	console.log("booom!");
+});
